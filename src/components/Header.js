0cc18862c3268b0ab5c3053ec5440505a71328c8
Rohan <rohan@hh>
2023-04-18 18:41:37 +0530

    header component added

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = ()=> {
+return (
+    <div className="ui menu center">
+        <div className="ui container center">
+        <h2>Contact Me</h2>
+        </div>
+    </div>
+)
+}
+
+export default Header;
